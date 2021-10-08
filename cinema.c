ba2e927338f6905e90d227bba1bc91abce1fad95
Daymortel <daymortel@gmail.com>
2021-10-08 12:04:49 +0200

    Update cinema.c

--- cinema.c
+++ cinema.c
@@ -2,68 +2,67 @@
 
 int main(void)
 {
-	char *filmCategorieEnfant[3];
-	// Tableau avec les 3 films
-	filmCategorieEnfant[0] = "Les télétobises";
-	filmCategorieEnfant[1] = "Dora l'exploratrice";
-	filmCategorieEnfant[2] = "Tchoupi et doudou";
-	char *filmCategorieAdos[3];
-	// Tableau avec les 3 films
-	filmCategorieAdos[0] = "Twilight";
-	filmCategorieAdos[1] = "Harry Potter";
-	filmCategorieAdos[2] = "Vampire diaries";
-	char *filmCategorieAdulte[3];
-	// Tableau avec les 3 films
-	filmCategorieAdulte[0] = "The Shining";
-	filmCategorieAdulte[1] = "La Nonne";
-	filmCategorieAdulte[2] = "Conjuring";
-	// Je demande ton âge
-	int age = printf("Quel âge as-tu ?\n");
-	scanf("%i", &age);
-	// Tu me dis ton âge
-	if(age < 11)
-	{
-		printf("Tu iras voir un film pour enfant\n");
-	}
-	else if(age >= 11 && age < 18)
-	{
-		printf("Tu iras voir un film pour adolescent\n");
-	}
-	else if(age >= 18)
-	{
-		for(int a = 0; a < 3; a++)
-		{
-			// Choix des films
-			int answer = printf("Veux-tu regarder %s ? [oui(1)/non(2)]\n", filmCategorieAdulte[a]);
-			scanf("%i", &answer);
-			if(answer == 1)
-			{
-				printf("Bon film !\n");
-				return 0;
-			}
-		}
-		for(int a = 0; a < 3; a++)
-		{
-			// Choix des films
-			int answer = printf("Veux-tu regarder %s ? [oui(1)/non(2)]\n", filmCategorieAdos[a]);
-			scanf("%i", &answer);
-			if(answer == 1)
-			{
-				printf("Bon film !\n");
-				return 0;
-			}
-		}
-		for(int a = 0; a < 3; a++)
-		{
-			// Choix des films
-			int answer = printf("Veux-tu regarder %s ? [oui(1)/non(2)]\n", filmCategorieEnfant[a]);
-			scanf("%i", &answer);
-			if(answer == 1)
-			{
-				printf("Bon film !\n");
-				return 0;
-			}
-		}
-	}
-	system("PAUSE");
+    // Tableau avec les 3 films pour enfants
+    char *filmCategorieEnfant[3];
+    filmCategorieEnfant[0] = "Les Télétobises";
+    filmCategorieEnfant[1] = "Dora l'exploratrice";
+    filmCategorieEnfant[2] = "Tchoupi";
+    // Tableau avec les 3 films pour ados
+    char *filmCategorieAdos[3];
+    filmCategorieAdos[0] = "Twilight";
+    filmCategorieAdos[1] = "Harry Potter";
+    filmCategorieAdos[2] = "Vampire diaries";
+    // Tableau avec les 3 films pour adultes
+    char *filmCategorieAdulte[3];
+    filmCategorieAdulte[0] = "The Shining";
+    filmCategorieAdulte[1] = "La Nonne";
+    filmCategorieAdulte[2] = "Conjuring";
+    // Je demande ton âge
+    int age = printf("Quel âge as-tu ?\n");
+    scanf("%i", &age);
+    // Tu me dis ton âge
+    if (age < 11)
+    {
+        printf("Tu iras voir un film pour enfant\n");
+    }
+    else if (age >= 11 && age < 18)
+    {
+        printf("Tu iras voir un film pour adolescent\n");
+    }
+    else if (age >= 18)
+    {
+        for (int a = 0; a < 3; a++)
+        {
+            // Choix des films pour adultes
+            int answer = printf("Veux-tu regarder %s ? [oui(1)/non(2)]\n", filmCategorieAdulte[a]);
+            scanf("%i", &answer);
+            if (answer == 1)
+            {
+                printf("Bon film !\n");
+                return 0;
+            }
+        }
+        for (int a = 0; a < 3; a++)
+        {
+            // Choix des films pour ados
+            int answer = printf("Veux-tu regarder %s ? [oui(1)/non(2)]\n", filmCategorieAdos[a]);
+            scanf("%i", &answer);
+            if (answer == 1)
+            {
+                printf("Bon film !\n");
+                return 0;
+            }
+        }
+        for (int a = 0; a < 3; a++)
+        {
+            // Choix des films pour enfants
+            int answer = printf("Veux-tu regarder %s ? [oui(1)/non(2)]\n", filmCategorieEnfant[a]);
+            scanf("%i", &answer);
+            if (answer == 1)
+            {
+                printf("Bon film !\n");
+                return 0;
+            }
+        }
+    }
 }
